25a49139bed560912ce5a762aa0b2b88c413d209
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-10-18 19:53:17 +0530

    [ugesh] First Commit

---
+++ Day15 & 16_PracticeProblem/FindNearest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15___16_PracticeProblem
+{
+    internal class FindNearest
+    {
+        public int FindClosestEvenNumber(int N)
+        {
+            int closestEvenNumber = 0;
+
+            // Find the closest even number greater than or equal to N
+            if (N % 2 == 0)
+            {
+                closestEvenNumber = N;
+            }
+            else
+            {
+                closestEvenNumber = (N > 0) ? N + 1 : N - 1;
+            }
+
+            while (!IsAllDigitsEven(closestEvenNumber))
+            {
+                closestEvenNumber = (N > 0) ? closestEvenNumber + 2 : closestEvenNumber - 2;
+            }
+
+            return closestEvenNumber;
+        }
+
+        private bool IsAllDigitsEven(int num)
+        {
+            string numStr = Math.Abs(num).ToString();
+            return numStr.All(c => (c - '0') % 2 == 0);
+        }
+    }
+}
---
+++ Day15 & 16_PracticeProblem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+
+namespace Day15___16_PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindNearest quiz = new FindNearest();
+            int N = 17;
+
+            int closestEven = quiz.FindClosestEvenNumber(N);
+            Console.WriteLine($"Closest even number to {N} is {closestEven}");
+
+            Type quizType = typeof(FindNearest);
+            Console.WriteLine("Class Members:");
+            foreach (MemberInfo member in quizType.GetMembers())
+            {
+                Console.WriteLine(member.Name);
+            }
+
+            object emptyObj = Activator.CreateInstance(quizType);
+            Console.WriteLine("Empty Object Created");
+
+            object[] constructorArgs = new object[] { 42 };
+            object parameterizedObj = Activator.CreateInstance(quizType, constructorArgs);
+            Console.WriteLine("Parameterized Object Created");
+
+            MethodInfo methodInfo = quizType.GetMethod("FindClosestEvenNumber");
+            object[] methodArgs = new object[] { 33 };
+            int result = (int)methodInfo.Invoke(parameterizedObj, methodArgs);
+            Console.WriteLine($"Invoked method FindClosestEvenNumber(33) and got result: {result}");
+        }
+    }
+}
